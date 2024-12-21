3bf88936f90fad2772895ab7525de183306362cf
Cory Sohrakoff <2347328+sohrak@users.noreply.github.com>
2024-12-21 18:16:23 +0000

    Add file reader for day 5

--- s5.py
+++ s5.py
@@ -3,9 +3,34 @@
 from collections import defaultdict
 from common import *
 
+ORDER_SEPARATOR = '|'
+UPDATE_SEPARATOR = ','
+
+
+def get_print_instructions():
+    ordering = defaultdict(list)
+    updates = []
+
+    for line in get_lines(strip_newlines=True):
+        if ORDER_SEPARATOR in line:
+            before, after = map(int, line.split(ORDER_SEPARATOR))
+            ordering[before].append(after)
+        elif UPDATE_SEPARATOR in line:
+            updates.append([int(x) for x in line.split(UPDATE_SEPARATOR)])
+
+    return ordering, updates
+
 
 def part1():
-    print('Part 1:')
+    ordering, updates = get_print_instructions()
+    count = 0
+
+    for update in updates:
+        # for each update
+        # check that pages follow the rules
+        pass
+
+    print('Part 1:', count)
 
 
 def part2():
