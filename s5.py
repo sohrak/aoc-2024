a0d9f9526830913f685a7f34ea14afed32037f14
Cory Sohrakoff <2347328+sohrak@users.noreply.github.com>
2024-12-21 17:54:20 +0000

    Add day 5 skeleton

---
+++ s5.py
@@ -0,0 +1,17 @@
+# https://adventofcode.com/2024/day/5
+
+from collections import defaultdict
+from common import *
+
+
+def part1():
+    print('Part 1:')
+
+
+def part2():
+    print('Part 2:')
+
+
+if __name__ == '__main__':
+    part1()
+    part2()
